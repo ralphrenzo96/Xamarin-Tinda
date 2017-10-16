516ccd40a62220b2ccc5ad9308192488caa68c5f
Ralph Renzo Pitogo <ralph.pitogo@appstone.tech>
2017-10-16 16:34:52 +0800

    Added custom template

--- tinda/Utilities/Constants.cs
+++ tinda/Utilities/Constants.cs
@@ -7,6 +7,6 @@ namespace tinda.Utilities
     {
         public static Color THEME_COLOR = Color.FromHex("00CECC");
 
-        public static double DEFAULT_TEMPLATE_HEIGHT = Device.RuntimePlatform == Device.Android ? 46 : 54;
+        public static double DEFAULT_TEMPLATE_HEIGHT = Device.RuntimePlatform == Device.Android ? 46 : 64;
     }
 }
--- tinda/Views/Details/CustomersPage.xaml.cs
+++ tinda/Views/Details/CustomersPage.xaml.cs
@@ -10,6 +10,15 @@ namespace tinda.Views.Details
 		public static readonly BindableProperty BindableNavTitle = BindableProperty.Create("NavTitle", typeof(string), typeof(CustomersPage), "Customers");
 		public string NavTitle { get { return (string)GetValue(BindableNavTitle); } }
 
+		public static readonly BindableProperty BindableNavIcon = BindableProperty.Create("NavIcon", typeof(string), typeof(CustomersPage), "people");
+		public string NavIcon { get { return (string)GetValue(BindableNavIcon); } }
+
+		public static readonly BindableProperty BindableNavLeftIcon = BindableProperty.Create("NavLeftIcon", typeof(string), typeof(CustomersPage), "burger");
+        public string NavLeftIcon { get { return (string)GetValue(BindableNavLeftIcon); } }
+
+		public static readonly BindableProperty BindableNavRightIcon = BindableProperty.Create("NavRightIcon", typeof(string), typeof(CustomersPage), "add");
+		public string NavRightIcon { get { return (string)GetValue(BindableNavRightIcon); } }
+
         public CustomersPage()
         {
             InitializeComponent();
