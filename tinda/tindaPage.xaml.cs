a0653877aa83ed0d85be837d400cc8cf553bdc67
Ralph Renzo Pitogo <ralph.pitogo@appstone.tech>
2017-10-16 14:06:33 +0800

    Xamarin Tinda

---
+++ tinda/tindaPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace tinda
+{
+    public partial class tindaPage : ContentPage
+    {
+        public tindaPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
